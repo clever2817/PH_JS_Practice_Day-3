9f2be9e1c0606ceb6242ab97a8615c84dd8e0b3e
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-30 11:38:02 +0600

    odd_even.js subtask-2 solved using while loop

--- while/2_odd_even.js
+++ while/2_odd_even.js
@@ -13,3 +13,20 @@ while(num <= 100)
 
     num++;
 }
+
+
+// Subtask-2
+
+// Find all the even numbers from 78 to 98
+
+let number = 78;
+
+while(number <= 98)
+ {
+    if(number % 2 === 0)
+    {
+        console.log(number);
+    }
+
+    number++;
+ }
