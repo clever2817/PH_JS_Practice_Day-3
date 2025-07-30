d310603acec1088fc504b03779f63354c573f948
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-30 12:14:13 +0600

    odd_even.js subtask-2 solved using for loop

--- for/2_odd_even.js
+++ for/2_odd_even.js
@@ -9,3 +9,16 @@ for(let i=61; i<=100; i++)
         console.log(i);
     }
 }
+
+
+// Subtask-2
+
+// Find all the even numbers from 78 to 98
+
+for(let i=78; i<=98; i++)
+{
+    if(i % 2 === 0)
+    {
+        console.log(i);
+    }
+}
