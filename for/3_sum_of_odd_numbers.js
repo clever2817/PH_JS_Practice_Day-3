ea2a07b88863f8a732843cb01ccd60e168893d75
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-30 12:22:44 +0600

    sum_of_odd_numbers.js subtask-2 solved using for loop

--- for/3_sum_of_odd_numbers.js
+++ for/3_sum_of_odd_numbers.js
@@ -13,3 +13,20 @@ for(let i=91; i<=129; i++)
 }
 
 console.log("Sum of odd numbers from 91 to 129 is : ", sum);
+
+
+// Subtask-2
+
+// Display sum of all the even numbers from 51 to 85.
+
+let total = 0;
+
+for(let i=51; i<=85; i++)
+{
+    if(i % 2 === 0)
+    {
+        total += i;
+    }
+}
+
+console.log("sum of all the even numbers from 51 to 85 is: ", total);
